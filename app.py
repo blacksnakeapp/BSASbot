de93cd2963a556e52fc2045c84063456711c8356
blacksnakeapp <blacksnakeapp@gmail.com>
2025-08-10 20:55:08 +0700

    py

--- app.py
+++ app.py
@@ -46,15 +46,17 @@ def handle_message():
             # --- LOGIKA CHATBOT ANDA DIMULAI DI SINI ---
             balasan = f"Anda bilang: '{message_text}'. Saya bot sederhana!"
             if "halo" in message_text.lower():
-                balasan = "Halo kembali dari bot!"
-            if "info produk" in message_text:
-                balasan = "Tentu, kami menjual Produk A (Rp 50rb) dan Produk B (Rp 75rb). Anda tertarik yang mana?"
-            elif "jam buka" in message_text:
-                balasan = "Toko kami buka setiap hari dari jam 9 pagi sampai jam 5 sore."
+                balasan = "Halo kembali dari BSAS-BOT GANTENG!"
+            if "BSAS" in message_text:
+                balasan = "Tentu, kami menyediakan jasa pembuatan Custom App, Web Builder, Server Managed DLL, Anda tertarik yang mana?"
+            elif "TONGSOLOP" in message_text:
+                balasan = "Oh itu akun live kami tentang Game, kalian bisa follow tiktok kami di https://www.tiktok.com/@tongsolop untuk konten menarik"
             elif "terima kasih" in message_text:
                 balasan = "Sama-sama! Senang bisa membantu."
+            elif "BLACKAXE" in message_text:
+                balasan = "BLACK AXE adalah grup whatsapp kami yang berisi para orang ganteng seperti, Rendy Mambo sang pelor, Danny Cagur sang Play-Lor, Raffles Rooftop sang pengendali Angin, Hans Bara Bere sang dewa Galau dam tentunya Dewa Ular Hitam sang pawang Ular Hitam kecil imut yang lucu, Apakah informasi ini membantu?"
             else:
-                balasan = f"Maaf, saya belum mengerti maksud dari '{message_text}'. Coba tanya tentang 'info produk' atau 'jam buka'."
+                balasan = f"Maaf, saya belum mengerti maksud dari '{message_text}'. Coba tanya tentang 'BSAS' atau 'TONGSOLOP' atau 'BLACKAXE'."
 
             send_whatsapp_message(from_number, balasan)
     except Exception as e:
